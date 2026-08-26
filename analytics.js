6eeaab6c00f70f7cda742101f7abedc5439b039e
Doug Scott <doug.scott@asapventures.com>
2026-08-26 07:04:38 +0100

    Recover compiled site baseline

---
+++ analytics.js
@@ -0,0 +1,24 @@
+(() => {
+  "use strict";
+  const measurementId = "G-X8V7G1S3EH";
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function gtag() { window.dataLayer.push(arguments); };
+  window.gtag("consent", "default", {
+    analytics_storage: "denied",
+    ad_storage: "denied",
+    ad_user_data: "denied",
+    ad_personalization: "denied"
+  });
+  window.gtag("consent", "update", {
+    analytics_storage: "granted",
+    ad_storage: "denied",
+    ad_user_data: "denied",
+    ad_personalization: "denied"
+  });
+  window.gtag("js", new Date());
+  window.gtag("config", measurementId, { send_page_view: true });
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  document.head.appendChild(script);
+})();
